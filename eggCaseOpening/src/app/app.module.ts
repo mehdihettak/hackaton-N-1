e084050a97ed895645f563fc343ab13725504781
mehdi hettak <vics06@hotmail.fr>
2019-04-18 14:11:49 +0200

    debut

---
+++ eggCaseOpening/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CasesComponent } from './cases/cases.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CasesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
